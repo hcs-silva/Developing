013846417c17615019f5f28c4dc87b032c20476e
Hernâni Silva <hcsilvamatos@hotmail.com>
2024-07-07 09:57:54 +0100

    adicionada navegação entre páginas, event listeners para os botões, funções de Clear para cada botão. corrigidos estilos

--- Salários/js/scripts.js
+++ Salários/js/scripts.js
@@ -61,6 +61,10 @@ const subsidioCompletoBox = document.querySelector("#subsidioCompleto");
 const contratoFull = document.querySelector("#full-time");
 const contratoPart = document.querySelector("#part-time");
 const numberInputs = document.querySelectorAll('input[type="number"]');
+const valorAlim = document.querySelector("#diarioAlim");
+const recebeSubAlim = document.querySelector("#recebe");
+const naoRecebeSubAlim = document.querySelector("#nao-recebe");
+const valorAlimBox = document.querySelector("#diarioAlimBox");
 
 //Seleção de páginas
 
@@ -75,7 +79,12 @@ const calcBtnPageOne = document.querySelector("#calc-btn-pageOne");
 const calcBtnPageTwo = document.querySelector("#calc-btn-pageTwo");
 const backBtn = document.querySelector("#back-btn");
 const backBtnPageTwo = document.querySelector("#back-btn-pageTwo");
+const backBtnPageThree = document.querySelector("#nack-btn-pageThree");
+const backBtnResult = document.querySelector("#back-btn-result");
 const clearBtn = document.querySelector("#clear-btn");
+const clearBtnPageTwo = document.querySelector("#clear-btn-pageTwo");
+const clearBtnPageThree = document.querySelector("#clear-btn-pageThree");
+const restart = document.querySelector("#restart");
 
 // Funções
 function duodecimos(vencBase) {
@@ -152,6 +161,18 @@ function subsidioRecebido(vencBase) {
   return subsidio;
 }
 
+// Toggle do campo de valor diario do subsidio de alimentação
+
+function toggleAlim() {
+  if (recebeSubAlim.checked && !naoRecebeSubAlim.checked) {
+    valorAlimBox.classList.remove("hide");
+  }
+
+  if (!recebeSubAlim.checked && naoRecebeSubAlim.checked) {
+    valorAlimBox.classList.add("hide");
+  }
+}
+
 //Toggle form horas
 function toggleHoras() {
   if (fezHoras.checked && !naoFezHoras.checked) {
@@ -211,11 +232,13 @@ function horasNoite(horasNoturnas) {
   let nightHours = 0;
   if (porTurnos.checked && !semTurnos.checked) {
     horasNoturnasBox.classList.remove("hide");
+    noturnas.classList.remove("hide");
     nightHours = horasNoturnas * 1.25;
   }
 
   if (!porTurnos.checked && semTurnos.checked) {
     horasNoturnasBox.classList.add("hide");
+    noturnas.classList.add("hide");
     nightHours = 0;
   }
 
@@ -275,7 +298,12 @@ function calcular() {
   const horas75Val = parseFloat(horas75.value);
   const horas100Val = parseFloat(horas100.value);
 
-  const totalHoras = horasExtraordinarias(horas50Val, horas75Val, horas100Val, custoHora);
+  const totalHoras = horasExtraordinarias(
+    horas50Val,
+    horas75Val,
+    horas100Val,
+    custoHora
+  );
 
   const nightHours = horasNoite(horasNoiteVal); // Properly calculate night hours
 
@@ -283,15 +311,33 @@ function calcular() {
   const naoRem = parseFloat(naoRemunerado(absences, custoHora));
 
   const vencBruto = parseFloat(
-    (base + duodec * 2 + commission + totalHoras + nightHours + subsidio - absences).toFixed(2)
+    (
+      base +
+      duodec * 2 +
+      commission +
+      totalHoras +
+      nightHours +
+      subsidio -
+      absences
+    ).toFixed(2)
   );
 
-  const taxBracket = taxdata.find((item) => vencBruto > item.min && vencBruto <= item.max);
+  const taxBracket = taxdata.find(
+    (item) => vencBruto > item.min && vencBruto <= item.max
+  );
   const irs = taxBracket ? vencBruto * taxBracket.taxa : 0;
   const segSocial = vencBruto * 0.11;
 
   const totalDesc = (irs + segSocial).toFixed(2);
-  const vencFinal = vencLiquido(base, duodec, naoRem, commission, subsidio, totalHoras, nightHours);
+  const vencFinal = vencLiquido(
+    base,
+    duodec,
+    naoRem,
+    commission,
+    subsidio,
+    totalHoras,
+    nightHours
+  );
 
   duodecimoNatal.textContent = duodec;
   duodecimoFerias.textContent = duodec;
@@ -307,9 +353,33 @@ function calcular() {
 }
 
 function clearAll() {
-  vencBase.value = "";
-  comission.value = "";
-  faltas.value = "";
+  vencBase.value = 0;
+  comission.value = 0;
+  faltas.value = 0;
+  horasTrabalhadas.value = 0;
+  horas50.value = 0;
+  horas75.value = 0;
+  horas100.value = 0;
+  valorAlim.value = 0;
+  horasNoturnas.value = 0;
+}
+
+function clearPageOne() {
+  horasNoturnas.value = 0;
+  vencBase.value = 0;
+  valorAlim.value = 0;
+}
+
+function clearPageTwo() {
+  horasTrabalhadas.value = 0;
+  horas50.value = 0;
+  horas75.value = 0;
+  horas100.value = 0;
+}
+
+function clearPageThree() {
+  comission.value = 0;
+  faltas.value = 0;
 }
 
 //Eventos
@@ -318,7 +388,7 @@ calcBtn.addEventListener("click", (e) => {
 
   //Verificar validade do formulário
   if (form.checkValidity()) {
-    calcContainer.classList.add("hide");
+    pageThree.classList.add("hide");
     result.classList.remove("hide");
     calcular();
   } else {
@@ -351,19 +421,57 @@ naoFezHoras.addEventListener("change", () => {
   toggleHoras();
 });
 
-backBtn.addEventListener("click", () => {
-  calcContainer.classList.remove("hide");
+recebeSubAlim.addEventListener("change", () => {
+  toggleAlim();
+});
+
+naoRecebeSubAlim.addEventListener("change", () => {
+  toggleAlim();
+});
+
+backBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  pageTwo.classList.remove("hide");
+  pageThree.classList.add("hide");
+});
+
+backBtnPageTwo.addEventListener("click", (e) => {
+  e.preventDefault();
+  pageTwo.classList.add("hide");
+  pageOne.classList.remove("hide");
+});
+
+backBtnResult.addEventListener("click", (e) => {
+  e.preventDefault();
   result.classList.add("hide");
+  pageThree.classList.remove("hide");
 });
 
-clearBtn.addEventListener("click", () => {
-  clearAll();
+clearBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  clearPageThree();
 });
 
+clearBtnPageTwo.addEventListener("click", (e) => {
+  e.preventDefault();
+  clearPageTwo();
+});
 
 calcBtnPageOne.addEventListener("click", (e) => {
   e.preventDefault();
-    pageOne.classList.add("hide");
-    pageTwo.classList.remove("hide");
-})
+  pageOne.classList.add("hide");
+  pageTwo.classList.remove("hide");
+});
 
+calcBtnPageTwo.addEventListener("click", (e) => {
+  e.preventDefault();
+  pageTwo.classList.add("hide");
+  pageThree.classList.remove("hide");
+});
+
+restart.addEventListener("click", (e) => {
+  e.preventDefault();
+  clearAll();
+  result.classList.add("hide");
+  pageOne.classList.remove("hide");
+});
