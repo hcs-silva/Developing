8e80a8d3f655f30b9663285f89f2631795162c6e
Hernâni Silva <hcsilvamatos@hotmail.com>
2024-08-13 21:49:12 +0100

    Adicionadas restantes tabelas de descontos de IRS

--- Salários/js/scripts.js
+++ Salários/js/scripts.js
@@ -1,18 +1,94 @@
 // Data
-const taxdata = [
-  { min: 0, max: 820, taxa: 0 },
-  { min: 820, max: 935, taxa: 0.059 },
-  { min: 935, max: 1001, taxa: 0.083 },
-  { min: 1001, max: 1123, taxa: 0.094 },
-  { min: 1123, max: 1765, taxa: 0.154 },
-  { min: 1765, max: 2057, taxa: 0.179 },
-  { min: 2057, max: 2664, taxa: 0.222 },
-  { min: 2664, max: 3193, taxa: 0.25 },
-  { min: 3193, max: 4173, taxa: 0.285 },
-  { min: 4173, max: 5470, taxa: 0.315 },
-  { min: 5470, max: 6540, taxa: 0.333 },
-  { min: 6540, max: 20067, taxa: 0.412 },
+//Não casado sem dependentes ou casado dois titulares
+const tableOne = [
+  { min: 0, max: 820, taxa: 0, parcela: 0 },
+  { min: 820, max: 935, taxa: 0.059, parcela: 21.43 },
+  { min: 935, max: 1001, taxa: 0.083, parcela: 21.43 },
+  { min: 1001, max: 1123, taxa: 0.094, parcela: 21.43 },
+  { min: 1123, max: 1765, taxa: 0.154, parcela: 21.43 },
+  { min: 1765, max: 2057, taxa: 0.179, parcela: 21.43 },
+  { min: 2057, max: 2664, taxa: 0.222, parcela: 21.43 },
+  { min: 2664, max: 3193, taxa: 0.25, parcela: 21.43 },
+  { min: 3193, max: 4173, taxa: 0.285, parcela: 21.43 },
+  { min: 4173, max: 5470, taxa: 0.315, parcela: 21.43 },
+  { min: 5470, max: 6540, taxa: 0.333, parcela: 21.43 },
+  { min: 6540, max: 20067, taxa: 0.412, parcela: 21.43 },
 ];
+
+//Não casado com um ou mais dependentes
+const tableTwo = [
+  { min: 0, max: 820, taxa: 0, parcela: 0 },
+  { min: 820, max: 935, taxa: 0.022, parcela: 34.29 },
+  { min: 935, max: 1001, taxa: 0.049, parcela: 34.29 },
+  { min: 1001, max: 1123, taxa: 0.063, parcela: 34.29 },
+  { min: 1123, max: 1765, taxa: 0.135, parcela: 34.29 },
+  { min: 1765, max: 2057, taxa: 0.162, parcela: 34.29 },
+  { min: 2057, max: 2664, taxa: 0.21, parcela: 34.29 },
+  { min: 2664, max: 3193, taxa: 0.239, parcela: 34.29 },
+  { min: 3193, max: 4173, taxa: 0.277, parcela: 34.29 },
+  { min: 4173, max: 5470, taxa: 0.308, parcela: 34.29 },
+  { min: 5470, max: 6540, taxa: 0.328, parcela: 34.29 },
+  { min: 6540, max: 20067, taxa: 0.41, parcela: 34.29 },
+];
+
+//Casado único titular
+const tableThree = [
+  { min: 0, max: 857, taxa: 0, parcela: 0 },
+  { min: 857, max: 935, taxa: 0.04, parcela: 42.86 },
+  { min: 935, max: 1001, taxa: 0.058, parcela: 42.86 },
+  { min: 1001, max: 1393, taxa: 0.079, parcela: 42.86 },
+  { min: 1393, max: 1900, taxa: 0.107, parcela: 42.86 },
+  { min: 1900, max: 2801, taxa: 0.156, parcela: 42.86 },
+  { min: 2801, max: 3423, taxa: 0.179, parcela: 42.86 },
+  { min: 3423, max: 4099, taxa: 0.197, parcela: 42.86 },
+  { min: 4099, max: 5800, taxa: 0.235, parcela: 42.86 },
+  { min: 5800, max: 6422, taxa: 0.247, parcela: 42.86 },
+  { min: 6422, max: 20064.21, taxa: 0.368, parcela: 42.86 },
+];
+
+//Não casado ou casado dois titulares sem dependentes - Deficiente
+const tableFour = [
+  { min: 0, max: 1519.41, taxa: 0, parcela: 0 },
+  { min: 1519.41, max: 1648.29, taxa: 0.01, parcela: 0 },
+  { min: 1648.29, max: 1994.61, taxa: 0.048, parcela: 0 },
+  { min: 1994.61, max: 2410.71, taxa: 0.097, parcela: 0 },
+  { min: 2410.71, max: 4373.75, taxa: 0.219, parcela: 0 },
+  { min: 4373.75, max: 6621.18, taxa: 0.281, parcela: 0 },
+  { min: 6621.18, max: 6717.41, taxa: 0.283, parcela: 0 },
+  { min: 6717.41, max: 20264.85, taxa: 0.394, parcela: 0 },
+];
+
+//Não casado, com um ou mais dependentes - Deficiente
+
+const tableFive = [
+  { min: 0, max: 1677.09, taxa: 0, parcela: 0 },
+  { min: 1677.09, max: 1994.61, taxa: 0.015, parcela: 42.86 },
+  { min: 1994.61, max: 2410.71, taxa: 0.069, parcela: 42.86 },
+  { min: 2410.71, max: 4373.75, taxa: 0.204, parcela: 42.86 },
+  { min: 4373.75, max: 6621.18, taxa: 0.271, parcela: 42.86 },
+  { min: 6621.18, max: 6717.41, taxa: 0.273, parcela: 42.86 },
+  { min: 6717.41, max: 20264.85, taxa: 0.391, parcela: 42.86 },
+];
+
+//Casado dois titulares, com um ou mais dependentes - Deficiente
+const tableSix = [
+  { min: 0, max: 1574.66, taxa: 0, parcela: 0 },
+  { min: 1574.66, max: 1648.29, taxa: 0, parcela: 21.43 },
+  { min: 1648.29, max: 1994.61, taxa: 0.034, parcela: 21.43 },
+  { min: 1994.61, max: 2410.71, taxa: 0.085, parcela: 21.43 },
+  { min: 2410.71, max: 4373.75, taxa: 0.213, parcela: 21.43 },
+  { min: 4373.75, max: 6621.18, taxa: 0.276, parcela: 21.43 },
+  { min: 6621.18, max: 6717.41, taxa: 0.279, parcela: 21.43 },
+  { min: 6717.41, max: 20264.85, taxa: 0.393, parcela: 21.43 },
+];
+
+//Casado único titular - Deficiente
+const tableSeven = [
+  { min: 0, max: 2105.51, taxa: 0, parcela: 0 },
+  { min: 2105.51, max: 3622.95, taxa: 0.117, parcela: 42.86 },
+  { min: 3622.95, max: 6587.01, taxa: 0.213, parcela: 42.86 },
+  { min: 6587.01, max: 20264.85, taxa: 0.356, parcela: 42.86 },
+]
 
 // Seleção de Elementos
 
@@ -116,14 +192,14 @@ function duodecimos(vencBase) {
 function validateNumberInput(input) {
   if (input.type === "number") {
     const maxLength = 10;
-    let value = input.value;  
+    let value = input.value;
     const regex = /^\d{1,3}(?:[.,]\d+)?$/;
     if (!regex.test(value)) {
       value = value.replace(/[^\d.,]+/g, "");
       value = value.replace(",", ".");
     }
 
-    if(value.length > maxLength){
+    if (value.length > maxLength) {
       value = value.substring(0, maxLength);
     }
 
@@ -386,10 +462,10 @@ function vencLiquido(
     naoRemuneradoPart,
     naoRemuneradoFull
   );
-  const taxBracket = taxdata.find(
-    (item) => vencBruto > item.min && vencBruto <= item.max
-  );
-  const irs = taxBracket ? vencBruto * taxBracket.taxa : 0;
+  //const taxBracket = taxdata.find(
+  //  (item) => vencBruto > item.min && vencBruto <= item.max
+  //);
+  //const irs = taxBracket ? vencBruto * taxBracket.taxa : 0;
 
   const segSocial = vencBruto * 0.11;
 
